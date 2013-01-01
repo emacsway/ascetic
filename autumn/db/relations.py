d1b74f85635c57e247d9fd61d45b0f978aa2639e
EvoTech <ivzak@yandex.ru>
2013-01-01 02:20:03 +0200

    Relation refactory

--- autumn/db/relations.py
+++ autumn/db/relations.py
@@ -19,28 +19,36 @@ class Relation(RelationQSMixIn):
         self.field = field
         self.qs = qs
 
-    def _set_up(self, instance, owner):
+    def set_up(self, instance, owner):
         if isinstance(self.model, string_types):
             self.model = registry.get(self.model)
+
+    def set_field(self, model):
+        if self.field is None and model:
+            self.field = '{0}_id'.format(model._meta.db_table.split("_").pop())
 
 
 class ForeignKey(Relation):
 
+    def set_up(self, instance, owner):
+        super(ForeignKey, self).set_up(instance, owner)
+        self.set_field(self.model)
+
     def __get__(self, instance, owner):
-        super(ForeignKey, self)._set_up(instance, owner)
+        self.set_up(instance, owner)
         if not instance:
             return self.model
-        if not self.field:
-            self.field = '{0}_id'.format(self.model._meta.db_table.split("_").pop())
         return self.filter(**{self.model._meta.pk: getattr(instance, self.field)})[0]
 
 
 class OneToMany(Relation):
 
+    def set_up(self, instance, owner):
+        super(OneToMany, self).set_up(instance, owner)
+        self.set_field(instance)
+
     def __get__(self, instance, owner):
-        super(OneToMany, self)._set_up(instance, owner)
+        self.set_up(instance, owner)
         if not instance:
             return self.model
-        if not self.field:
-            self.field = '{0}_id'.format(instance._meta.db_table.split("_").pop())
         return self.filter(**{self.field: getattr(instance, instance._meta.pk)})
