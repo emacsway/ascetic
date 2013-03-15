70cf84fe07fc4e598a72bf14500766c206ec23de
EvoTech <ivzak@yandex.ru>
2013-03-15 07:23:33 +0200

    describe_table() in progress

--- autumn/db/connection.py
+++ autumn/db/connection.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+import re
 from threading import local
 from autumn.settings import DATABASES
 
@@ -86,6 +87,61 @@ class PostgreSQLDatabase(Database):
     def last_insert_id(self, cursor):
         cursor.execute("SELECT lastval()")
         return cursor.fetchone()[0]
+
+    def describe_table(self, table_name):
+        schema_name = None
+        schema_sql = schema_name and "AND n.nspname = {0}".format(schema_name) or ""
+        sql = """SELECT
+                a.attnum,
+                n.nspname,
+                c.relname,
+                a.attname AS colname,
+                t.typname AS type,
+                a.atttypmod,
+                FORMAT_TYPE(a.atttypid, a.atttypmod) AS complete_type,
+                d.adsrc AS default_value,
+                a.attnotnull AS notnull,
+                a.attlen AS length,
+                co.contype,
+                ARRAY_TO_STRING(co.conkey, ',') AS conkey
+            FROM pg_attribute AS a
+                JOIN pg_class AS c ON a.attrelid = c.oid
+                JOIN pg_namespace AS n ON c.relnamespace = n.oid
+                JOIN pg_type AS t ON a.atttypid = t.oid
+                LEFT OUTER JOIN pg_constraint AS co ON (co.conrelid = c.oid
+                    AND a.attnum = ANY(co.conkey) AND co.contype = 'p')
+                LEFT OUTER JOIN pg_attrdef AS d ON d.adrelid = c.oid AND d.adnum = a.attnum
+            WHERE a.attnum > 0 AND c.relname = {0}
+            {1}
+            ORDER BY a.attnum';
+        """.format(table_name, schema_sql)
+        cursor = self.cursor()
+        cursor.execute(sql)
+        fields = [f[0] for f in cursor.description]
+        desc = {}
+        for row in cursor.fetchall():
+            data = dict(list(zip(fields, row)))
+            default_value = data['default_value']
+            if data['type'] in ('varchar', 'bpchar'):
+                pass
+            d = {
+                'schema_name': data['nspname'],
+                'table_name': data['relname'],
+                'column_name': data['colname'],
+                'column_position': data['attnum'],
+                'data_type': data['type'],
+                'default': default_value,
+                'nullable': not data['notnull'],
+                'length': data['length'],
+                'scale': None,  # TODO
+                'precision': None,  # TODO
+                'unsigned': None,  # TODO
+                'pimary': None,
+                'primary_position': None,
+                'identity': None,
+            }
+            desc[data[colname]] = d
+        return desc
 
 for name, conf in DATABASES.items():
     connections[name] = Database.factory(**conf)
