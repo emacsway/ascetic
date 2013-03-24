daba78fba890d7ee4a15fc4678be5c8ad8f77b36
EvoTech <ivzak@yandex.ru>
2013-03-24 18:30:34 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # Copyright (c) 2012-2013 Ivan Zakrevsky, Jared Kuolt and contributors
+import os.path
 from setuptools import setup, find_packages
-from autumn import version
-
-version = '.'.join([str(x) for x in version])
 
 setup(
     name='autumn2',
@@ -13,11 +11,14 @@ setup(
 
     author="Ivan Zakrevsky",
     author_email="ivzak@yandex.ru",
-    description="A minimal ORM",
+    description="A minimal python ORM",
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url="https://bitbucket.org/evotech/autumn",
     license="MIT License",
     keywords = "ORM Database SQL",
+    install_requires=[
+        'sqlbuilder >= 0.7.1'
+    ],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
