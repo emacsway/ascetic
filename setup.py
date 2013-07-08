657424b25c6461c586693818f36df6fd85cddfcf
EvoTech <ivzak@yandex.ru>
2013-07-09 00:43:53 +0300

    0.6.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autumn2',
-    version='0.6.5.1',
+    version='0.6.6',
 
     packages = find_packages(),
     include_package_data=True,
