47241a988c963f2dff676d797c19c495c7a8afe5
EvoTech <ivzak@yandex.ru>
2014-01-10 02:58:49 +0200

    0.6.9.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autumn2',
-    version='0.6.9.1',
+    version='0.6.9.2',
 
     packages = find_packages(),
     include_package_data=True,
@@ -18,7 +18,7 @@ setup(
     license="MIT License",
     keywords = "ORM Database SQL",
     install_requires=[
-        'sqlbuilder >= 0.7.7'
+        'sqlbuilder >= 0.7.7.1'
     ],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
