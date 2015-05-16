14fc9a20353398e172a9415598c27a169a9bf084
emacsway <ivzak@yandex.ru>
2015-05-17 02:41:09 +0300

    gfk

--- autumn/contrib/gfk.py
+++ autumn/contrib/gfk.py
@@ -1,69 +1,68 @@
-from .. import models
+from ..models import ForeignKey, OneToMany, cascade, registry, to_tuple
 
 # Under construction!!! Not testet yet!!!
 
 
-class GenericForeignKey(models.Relation):
+class GenericForeignKey(ForeignKey):
 
-    def __init__(self, type_field="object_type_id", field="object_id"):
-        self.type_field = type_field
-        self.field = field
+    def __init__(self, type_field="object_type_id", rel_field=None, field=None, on_delete=cascade, rel_name=None):
+        self._type_field = type_field
+        self._rel_field = rel_field
+        self._field = field
+        self.on_delete = on_delete
+        self._rel_name = rel_name
 
-    def add_to_class(self, model_class, name):
-        self.model = model_class
-        self.name = name
-        self.model._meta.relations[name] = self
-        setattr(self.model, name, self)
+    @property
+    def rel_model(self):
+        raise AttributeError
+
+    @property
+    def add_related(self):
+        raise AttributeError
 
     def __get__(self, instance, owner):
         if not instance:
             return self
-        fk_val = getattr(instance, self.field)
         type_val = getattr(instance, self.type_field)
-        if fk_val is None:
+        rel_model = registry[type_val]
+        field = to_tuple(self.field(owner))
+        rel_field = to_tuple(self.rel_field(owner))
+        val = tuple(getattr(instance, f) for f in field)
+
+        if not [i for i in val if i is not None]:
             return None
-        cached_obj = instance._cache.get(self.name, None)
-        if (cached_obj is not None or cached_obj.pk != fk_val or
-                type(cached_obj)._meta.name != type_val):
-            rel_model = models.registry[type_val]
-            instance._cache[self.name] = rel_model.q.where(
-                rel_model.s.pk == fk_val
-            )
-        return instance._cache[self.name]
+
+        cached_obj = instance._cache.get(self.name(owner), None)
+        if not isinstance(cached_obj, rel_model) or tuple(getattr(cached_obj, f, None) for f in self.rel_field(owner)) != val:
+            t = self.rel_model._gateway.sql_table
+            q = self.rel_model._gateway.base_query
+            for f, v in zip(rel_field, val):
+                q = q.where(t.__getattr__(f) == v)
+            q = self._do_query(q)
+            instance._cache[self.name(owner)] = q[0]
+        return instance._cache[self.name(owner)]
 
     def __set__(self, instance, value):
-        instance._cache[self.name] = value
-        setattr(instance, self.field, value._get_pk())
         setattr(instance, self.type_field, type(value)._meta.name)
+        super(GenericForeignKey, self).__set__(instance, value)
 
     def __delete__(self, instance):
-        instance._cache.pop(self.name, None)
-        setattr(instance, self.field, None)
         setattr(instance, self.type_field, None)
+        super(GenericForeignKey, self).__delete__(instance)
 
 
-class GenericRelation(models.OneToMany):
+class GenericRelation(OneToMany):
 
-    @property
-    def rel_name(self):
-        if self._rel_name:
-            return self._rel_name
-        for rel in self.rel_model._meta.relations:
-            if rel.rel_model is self.model:
-                return rel.name
+    def rel_field(self, owner):
+        return getattr(self.rel_model(owner), self.rel_name(owner)).field
 
     @property
-    def rel_field(self):
-        return getattr(self.rel_model, self.rel_name).field
-
-    @property
-    def rel_type_field(self):
-        return getattr(self.rel_model, self.rel_name).type_field
+    def rel_type_field(self, owner):
+        return getattr(self.rel_model(owner), self.rel_name(owner))._type_field
 
     def __get__(self, instance, owner):
         if not instance:
             return self
-        return self.filter(**{
-            self.rel_field: getattr(instance, self.field),
-            self.rel_type_field: type(instance)._meta.name
-        })
+        q = super(GenericRelation, self).__get__(instance, owner)
+        t = self.rel_model(owner)._gateway.sql_table
+        return q.where(t.__getattr__(self.rel_type_field) == type(instance)._gateway.name)
