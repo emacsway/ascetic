ee8fa043d96b0e55a93a87e10a19327bd4253d95
emacsway <ivzak@yandex.ru>
2015-05-25 18:28:11 +0300

    0.7.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autumn2',
-    version='0.7.2.4',
+    version='0.7.2.5',
 
     packages = find_packages(),
     include_package_data=True,
