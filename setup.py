676b7be48926e6a7690de1d127bc71d1d0ee3852
emacsway <ivzak@yandex.ru>
2015-10-05 01:20:48 +0300

    sqlbuilder >= 0.7.9.16

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     license="MIT License",
     keywords = "ORM Database SQL",
     install_requires=[
-        'sqlbuilder >= 0.7.9.15'
+        'sqlbuilder >= 0.7.9.16'
     ],
     classifiers = [
         'Development Status :: 1 - Planning',
