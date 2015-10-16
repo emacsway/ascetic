9fbc0ec69624bcc2f7840fa74d7e24cc738d96c7
emacsway <ivzak@yandex.ru>
2015-10-16 17:28:12 +0300

    0.7.2.25

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ascetic',
-    version='0.7.2.24',
+    version='0.7.2.25',
 
     packages = find_packages(),
     include_package_data=True,
@@ -18,7 +18,7 @@ setup(
     license="MIT License",
     keywords = "ORM Database SQL",
     install_requires=[
-        'sqlbuilder >= 0.7.9.16'
+        'sqlbuilder >= 0.7.9.17'
     ],
     classifiers = [
         'Development Status :: 1 - Planning',
