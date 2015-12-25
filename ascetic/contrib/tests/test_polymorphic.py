7e221df6a3e6f7a780af2950136133ac9529a724
emacsway <ivzak@yandex.ru>
2015-12-25 21:07:06 +0200

    Used DataMapper interface instaed of ActiveRecord in test_polymorphic.py

--- ascetic/contrib/tests/test_polymorphic.py
+++ ascetic/contrib/tests/test_polymorphic.py
@@ -2,7 +2,7 @@ import unittest
 from ascetic import validators
 from ascetic.databases import databases
 from ascetic.contrib.polymorphic import PolymorphicMapper
-from ascetic.models import Model, ForeignKey, IdentityMap, mapper_registry, ObjectDoesNotExist
+from ascetic.models import Model, Mapper, ForeignKey, IdentityMap, mapper_registry, ObjectDoesNotExist
 
 Author = Book = Nonfiction = Avia = None
 
@@ -128,41 +128,77 @@ class TestPolymorphic(unittest.TestCase):
     @classmethod
     def create_models(cls):
 
-        class Author(Model):
+        class Author(object):
+            def __init__(self, id=None, lang=None, first_name=None, last_name=None, bio=None):
+                self.id = id
+                self.lang = lang
+                self.first_name = first_name
+                self.last_name = last_name
+                self.bio = bio
 
-            class Mapper(object):
-                db_table = 'ascetic_polymorphic_author'
-                defaults = {'bio': 'No bio available'}
-                validations = {'first_name': validators.Length(),
-                               'last_name': (validators.Length(), lambda x: x != 'BadGuy!' or 'Bad last name', )}
+        class AuthorMapper(Mapper):
+            db_table = 'ascetic_polymorphic_author'
+            defaults = {'bio': 'No bio available'}
+            validations = {'first_name': validators.Length(),
+                           'last_name': (validators.Length(), lambda x: x != 'BadGuy!' or 'Bad last name', )}
 
-        class Book(Model):
-            author = ForeignKey(
-                Author,
-                rel_field=('id', 'lang'),
-                field=('author_id', 'lang'),
-                rel_name='books',
-                rel_query=(lambda rel: mapper_registry[rel.rel_model].query)
-            )
+        AuthorMapper(Author)
 
-            class Mapper(PolymorphicMapper):
-                name = 'ascetic.contrib.tests.test_polymorphic.Book'
-                db_table = 'ascetic_polymorphic_book'
-                polymorphic = True
+        class Book(object):
+
+            def __init__(self, id=None, lang=None, polymorphic_type_id=None, title=None, author_id=None):
+                self.id = id
+                self.lang = lang
+                self.polymorphic_type_id = polymorphic_type_id
+                self.title = title
+                self.author_id = author_id
+
+        class BookMapper(PolymorphicMapper, Mapper):
+            name = 'ascetic.contrib.tests.test_polymorphic.Book'
+            db_table = 'ascetic_polymorphic_book'
+            polymorphic = True
+
+            relationships = {
+                'author': ForeignKey(
+                    Author,
+                    rel_field=('id', 'lang'),
+                    field=('author_id', 'lang'),
+                    rel_name='books',
+                    rel_query=(lambda rel: mapper_registry[rel.rel_model].query)
+                )
+            }
+
+        BookMapper(Book)
 
         class Nonfiction(Book):
 
-            class Mapper(PolymorphicMapper):
-                name = 'ascetic.contrib.tests.test_polymorphic.Nonfiction'
-                db_table = 'ascetic_polymorphic_nonfiction'
-                polymorphic = True
+            def __init__(self, nonfiction_ptr_id=None, nonfiction_ptr_lang=None, branch=None, **kwargs):
+                super(Nonfiction, self).__init__(**kwargs)
+                self.nonfiction_ptr_id = nonfiction_ptr_id
+                self.nonfiction_ptr_lang = nonfiction_ptr_lang
+                self.branch = branch
+
+        class NonfictionMapper(PolymorphicMapper, Mapper):
+            name = 'ascetic.contrib.tests.test_polymorphic.Nonfiction'
+            db_table = 'ascetic_polymorphic_nonfiction'
+            polymorphic = True
+
+        NonfictionMapper(Nonfiction)
 
         class Avia(Nonfiction):
 
-            class Mapper(PolymorphicMapper):
-                name = 'ascetic.contrib.tests.test_polymorphic.Avia'
-                db_table = 'ascetic_polymorphic_avia'
-                polymorphic = True
+            def __init__(self, avia_ptr_id=None, avia_ptr_lang=None, model=None, **kwargs):
+                super(Avia, self).__init__(**kwargs)
+                self.avia_ptr_id = avia_ptr_id
+                self.avia_ptr_lang = avia_ptr_lang
+                self.model = model
+
+        class AviaMapper(PolymorphicMapper, Mapper):
+            name = 'ascetic.contrib.tests.test_polymorphic.Avia'
+            db_table = 'ascetic_polymorphic_avia'
+            polymorphic = True
+
+        AviaMapper(Avia)
 
         return locals()
 
@@ -180,16 +216,18 @@ class TestPolymorphic(unittest.TestCase):
             db.execute('DELETE FROM {0}'.format(db.qn(table)))
 
     def test_book(self):
+        author_mapper = mapper_registry[Author]
+        book_mapper = mapper_registry[Book]
         author = Author(
             id=1,
             lang='en',
             first_name='First name',
             last_name='Last name',
         )
-        author.save()
+        author_mapper.save(author)
         author_pk = (1, 'en')
-        author = Author.get(author_pk)
-        self.assertEqual(author.pk, author_pk)
+        author = author_mapper.get(author_pk)
+        self.assertEqual(author_mapper.get_pk(author), author_pk)
 
         book = Book(
             id=5,
@@ -197,29 +235,34 @@ class TestPolymorphic(unittest.TestCase):
             title='Book title'
         )
         book.author = author
-        book.save()
+        book_mapper.save(book)
         book_pk = (5, 'en')
-        book = Book.get(book_pk)
-        self.assertEqual(book.pk, book_pk)
-        self.assertEqual(book.author.pk, author_pk)
+        book = book_mapper.get(book_pk)
+        self.assertEqual(book_mapper.get_pk(book), book_pk)
+        self.assertEqual(author_mapper.get_pk(book.author), author_pk)
 
-        author = Author.get(author_pk)
-        self.assertEqual(author.books[0].pk, book_pk)
+        author = author_mapper.get(author_pk)
+        self.assertEqual(book_mapper.get_pk(author.books[0]), book_pk)
+        self.assertIsInstance(author.books[0], Book)
 
-        book.delete()
-        self.assertRaises(ObjectDoesNotExist, Book.get, book_pk)
+        book_mapper.delete(book)
+        self.assertRaises(ObjectDoesNotExist, book_mapper.get, book_pk)
 
     def test_nonfiction(self):
+        author_mapper = mapper_registry[Author]
+        nonfiction_mapper = mapper_registry[Nonfiction]
+        book_mapper = mapper_registry[Book]
+
         author = Author(
             id=1,
             lang='en',
             first_name='First name',
             last_name='Last name',
         )
-        author.save()
+        author_mapper.save(author)
         author_pk = (1, 'en')
-        author = Author.get(author_pk)
-        self.assertEqual(author.pk, author_pk)
+        author = author_mapper.get(author_pk)
+        self.assertEqual(author_mapper.get_pk(author), author_pk)
 
         nonfiction_data = dict(
             id=5,
@@ -229,39 +272,45 @@ class TestPolymorphic(unittest.TestCase):
         )
         nonfiction = Nonfiction(**nonfiction_data)
         nonfiction.author = author
-        nonfiction.save()
+        nonfiction_mapper.save(nonfiction)
         nonfiction_pk = (5, 'en')
 
-        nonfiction = Book.get(nonfiction_pk)
+        nonfiction = book_mapper.get(nonfiction_pk)
         self.assertIsInstance(nonfiction, Nonfiction)
         for k, v in nonfiction_data.items():
             self.assertEqual(getattr(nonfiction, k), v)
-        self.assertEqual(nonfiction.pk, nonfiction_pk)
-        self.assertEqual(nonfiction.author.pk, author_pk)
-        self.assertEqual(author.books[0], nonfiction)
+        self.assertEqual(nonfiction_mapper.get_pk(nonfiction), nonfiction_pk)
+        self.assertEqual(author_mapper.get_pk(nonfiction.author), author_pk)
+        self.assertEqual(book_mapper.get_pk(author.books[0]), nonfiction_mapper.get_pk(nonfiction))
+        self.assertIsInstance(author.books[0], Nonfiction)
 
-        nonfiction = Nonfiction.get(nonfiction_pk)
+        nonfiction = nonfiction_mapper.get(nonfiction_pk)
         self.assertIsInstance(nonfiction, Nonfiction)
         for k, v in nonfiction_data.items():
             self.assertEqual(getattr(nonfiction, k), v)
-        self.assertEqual(nonfiction.pk, nonfiction_pk)
-        self.assertEqual(nonfiction.author.pk, author_pk)
-        self.assertEqual(author.books[0], nonfiction)
+        self.assertEqual(nonfiction_mapper.get_pk(nonfiction), nonfiction_pk)
+        self.assertEqual(author_mapper.get_pk(nonfiction.author), author_pk)
+        self.assertEqual(book_mapper.get_pk(author.books[0]), nonfiction_mapper.get_pk(nonfiction))
+        self.assertIsInstance(author.books[0], Nonfiction)
 
-        nonfiction.delete()
-        self.assertRaises(ObjectDoesNotExist, Book.get, nonfiction_pk)
+        nonfiction_mapper.delete(nonfiction)
+        self.assertRaises(ObjectDoesNotExist, book_mapper.get, nonfiction_pk)
 
     def test_avia(self):
+        author_mapper = mapper_registry[Author]
+        avia_mapper = mapper_registry[Avia]
+        book_mapper = mapper_registry[Book]
+
         author = Author(
             id=1,
             lang='en',
             first_name='First name',
             last_name='Last name',
         )
-        author.save()
+        author_mapper.save(author)
         author_pk = (1, 'en')
-        author = Author.get(author_pk)
-        self.assertEqual(author.pk, author_pk)
+        author = author_mapper.get(author_pk)
+        self.assertEqual(author_mapper.get_pk(author), author_pk)
 
         avia_data = dict(
             id=5,
@@ -272,29 +321,31 @@ class TestPolymorphic(unittest.TestCase):
         )
         avia = Avia(**avia_data)
         avia.author = author
-        avia.save()
+        avia_mapper.save(avia)
         avia_pk = (5, 'en')
 
-        book = Book.q.where(Book.pk == avia_pk).polymorphic(False)[0]
+        book = book_mapper.query.where(book_mapper.sql_table.pk == avia_pk).polymorphic(False)[0]
         self.assertEqual(book.__class__, Book)
         avia = book.concrete_instance
         self.assertIsInstance(avia, Avia)
 
-        avia = Book.get(avia_pk)
+        avia = book_mapper.get(avia_pk)
         self.assertIsInstance(avia, Avia)
         for k, v in avia_data.items():
             self.assertEqual(getattr(avia, k), v)
-        self.assertEqual(avia.pk, avia_pk)
-        self.assertEqual(avia.author.pk, author_pk)
-        self.assertEqual(author.books[0], avia)
+        self.assertEqual(avia_mapper.get_pk(avia), avia_pk)
+        self.assertEqual(author_mapper.get_pk(avia.author), author_pk)
+        self.assertEqual(book_mapper.get_pk(author.books[0]), avia_mapper.get_pk(avia))
+        self.assertIsInstance(author.books[0], Avia)
 
-        avia = Avia.get(avia_pk)
+        avia = avia_mapper.get(avia_pk)
         self.assertIsInstance(avia, Avia)
         for k, v in avia_data.items():
             self.assertEqual(getattr(avia, k), v)
-        self.assertEqual(avia.pk, avia_pk)
-        self.assertEqual(avia.author.pk, author_pk)
-        self.assertEqual(author.books[0], avia)
+        self.assertEqual(avia_mapper.get_pk(avia), avia_pk)
+        self.assertEqual(author_mapper.get_pk(avia.author), author_pk)
+        self.assertEqual(book_mapper.get_pk(author.books[0]), avia_mapper.get_pk(avia))
+        self.assertIsInstance(author.books[0], Avia)
 
-        avia.delete()
-        self.assertRaises(ObjectDoesNotExist, Book.get, avia_pk)
+        avia_mapper.delete(avia)
+        self.assertRaises(ObjectDoesNotExist, book_mapper.get, avia_pk)
