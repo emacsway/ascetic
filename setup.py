2acb458073df9463b42b42cb18d1796cc73961f1
emacsway <ivzak@yandex.ru>
2015-12-26 05:28:55 +0200

    0.7.2.28

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ascetic',
-    version='0.7.2.27',
+    version='0.7.2.28',
 
     packages = find_packages(exclude=('examples*',)),
     include_package_data=True,
