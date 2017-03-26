1c7077851dcbb8dd8091f2acbf899a9bd892b347
emacsway <ivzak@yandex.ru>
2017-03-26 10:14:17 +0300

    Moved Signal() to WeakKeyDictionary()

--- ascetic/signals.py
+++ ascetic/signals.py
@@ -1,41 +1,35 @@
 from __future__ import absolute_import
-import random
-import threading
-from weakref import WeakValueDictionary
+from weakref import WeakKeyDictionary, WeakValueDictionary
 
 __all__ = ('Signal', 'pre_save', 'post_save', 'pre_delete', 'post_delete',
            'pre_init', 'post_init', 'class_prepared', 'field_mangling', 'column_mangling', )
 
 
-class Signal(object):
+class UndefinedSender(object):
+    pass
 
-    _clearing_frequency = 0.3
+undefined_sender = UndefinedSender()
+
+
+class Signal(object):
 
     def __init__(self):
         self._flush()
 
     def _flush(self):
-        self._receivers = {}
-        # Different senders can have the same hash-value.
-        # Hash can be primary key or something other.
-        # Eventually, sender can be non-hashable.
-        # So, we can't use sender as key in WeakKeyDictionary()
-        # We store sender in WeakValueDictionary() to track sender_id
-        self._senders = WeakValueDictionary()
+        self._receivers = WeakKeyDictionary()
         self._weak_cache = set()
 
     def connect(self, receiver, sender=None, weak=True, receiver_id=None):
+        if sender is None:
+            sender = undefined_sender
         if not weak:
             self._weak_cache.add(receiver)
         if receiver_id is None:
             receiver_id = self._make_id(receiver)
-        sender_id = self._make_id(sender)
-        if sender_id not in self._receivers:
-            self._receivers[sender_id] = WeakValueDictionary()
-            # self._senders holds link to hashable key of WeakKeyDictionary
-            self._senders[sender_id] = sender
-        self._receivers[sender_id][receiver_id] = receiver
-        self._clear_receivers()
+        if sender not in self._receivers:
+            self._receivers[sender] = WeakValueDictionary()
+        self._receivers[sender][receiver_id] = receiver
 
     @staticmethod
     def _make_id(target):
@@ -44,37 +38,31 @@ class Signal(object):
         return (type(target), id(target))
 
     def disconnect(self, receiver=None, sender=None, receiver_id=None):
+        if sender is None:
+            sender = undefined_sender
         if receiver_id is None:
             receiver_id = self._make_id(receiver)
         if receiver_id:
             try:
-                sender_id = self._make_id(sender)
-                self._weak_cache.discard(receiver or self._receivers[sender_id][receiver_id])
-                del self._receivers[sender_id][receiver_id]
-                if not self._receivers[sender_id]:
-                    del self._receivers[sender_id]
-                    del self._senders[sender_id]
+                self._weak_cache.discard(receiver or self._receivers[sender][receiver_id])
+                del self._receivers[sender][receiver_id]
+                if not self._receivers[sender]:
+                    del self._receivers[sender]
             except KeyError:
                 pass
         else:
             raise ValueError('a receiver or a receiver_id must be provided')
 
     def send(self, sender, *args, **kwargs):
+        if sender is None:
+            sender = undefined_sender
         responses = []
-        sender_id = self._make_id(sender)
-        if sender_id in self._receivers:
-            for receiver in self._receivers[sender_id].values():
+        if sender in self._receivers:
+            for receiver in self._receivers[sender].values():
                 responses.append((receiver, receiver(sender, *args, **kwargs)))
-        if sender is not None:
-            responses += self.send(None, *args, **kwargs)
+        if sender is not undefined_sender:
+            responses += self.send(undefined_sender, *args, **kwargs)
         return responses
-
-    def _clear_receivers(self):
-        if random.random() < self._clearing_frequency:
-            with threading.RLock():
-                for sender_id, receivers in list(self._receivers.items()):
-                    if not receivers or sender_id not in self._senders:
-                        del self._receivers[sender_id]
 
 
 def connect(signal, sender=None, weak=True, receiver_id=None):
